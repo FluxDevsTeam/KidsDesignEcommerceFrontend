3abbbec247a9086952510ec5070e92c755e37bad
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-08-28 22:44:25 +0100

    removed log

--- src/pages/orders/Contact.tsx
+++ src/pages/orders/Contact.tsx
@@ -12,12 +12,6 @@ const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID as string;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string;
 
-// Debug environment variables
-console.log('Service ID:', SERVICE_ID);
-console.log('Template ID:', TEMPLATE_ID);
-console.log('Public Key:', PUBLIC_KEY);
-console.log('Environment variables loaded:', !!SERVICE_ID && !!TEMPLATE_ID && !!PUBLIC_KEY);
-
 // Initialize EmailJS
 emailjs.init(PUBLIC_KEY);
 
